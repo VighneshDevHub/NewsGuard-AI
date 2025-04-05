7ebdfdc69d008c6dcefcf3d46a35f6f9ca7eeb89
Vighnesh Salunkhe <vighneshsalunkhe949@gmail.com>
2025-04-05 19:19:59 +0530

    add:configuration for app.py

---
+++ config.py
@@ -0,0 +1,14 @@
+class Config:
+    # Flask-Mail configuration
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'salunkhesantosh787@gmail.com'
+    MAIL_PASSWORD = 'ohjq iqcn zrpt xpco'
+
+    # Google Custom Search API configuration
+    GOOGLE_API_KEY = "AIzaSyB2nlYuSgnoKLBKC4aF2nfF2drE3ZWIMNk"
+    GOOGLE_CSE_ID = "15c198a8769a045ec"
+
+    
